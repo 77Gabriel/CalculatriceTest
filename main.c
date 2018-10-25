14c5d1b219417053570527cccf7d8f408182bb50
77Gabriel <44476244+77Gabriel@users.noreply.github.com>
2018-10-25 19:54:32 +0200

    Soustraction

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int addition(int, int);
+int soustraction(int, int);
 
 int main() {
 
     int a=1, b=4;
 
     addition(a,b);
-    printf("L'addition de a et b vaut %d", addition(a,b));
+    printf("Soustraction de a et b %d", soustraction(a,b));
     return 0;
 }
 
-int addition(a, b){
-    int resultat;
-    resultat = a+b;
-    return resultat;
+int soustraction(a, b){
+    return a-b;
 }
