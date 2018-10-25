8ee9d1e58d86bd294c3ea5642d371c20dd0aab8f
77Gabriel <44476244+77Gabriel@users.noreply.github.com>
2018-10-25 19:35:57 +0200

    New Code

---
+++ cmake-build-debug/CMakeFiles/CalculatriceTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculatriceTest.dir/main.c.obj"
+  "CalculatriceTest.pdb"
+  "CalculatriceTest.exe"
+  "CalculatriceTest.exe.manifest"
+  "libCalculatriceTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CalculatriceTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int addition(int, int);
+
+int main() {
+
+    int a=1, b=4;
+
+    addition(a,b);
+    printf("%d", addition(a,b));
+    return 0;
+}
+
+int addition(a, b){
+    int resultat;
+    resultat = a+b;
+    return resultat;
+}
